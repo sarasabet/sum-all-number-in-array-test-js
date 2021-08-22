01a19925ba5ec2321f08bdb78ab35e6a932663bf
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-22 03:18:00 +0000

    w1d5

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (const num of array) {
+    if (!Array.isArray(num)) {
+      sum += num
+    }
+    else{
+      sum += sumItems(num)
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
